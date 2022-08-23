e02a6df3695501d7a0fae2e0c3b13caeb01de2d0
AnnabelNKir <nkiroteannabel@gmail.com>
2022-08-23 11:15:53 +0300

    Updates

---
+++ precache-manifest.c52b84a13ab4e1f7f3ca3f0bf1936232.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7cb167f82f578502284",
+    "url": "/Poesy_challenge/static/css/main.fb4c2a56.chunk.css"
+  },
+  {
+    "revision": "d7cb167f82f578502284",
+    "url": "/Poesy_challenge/static/js/main.d7cb167f.chunk.js"
+  },
+  {
+    "revision": "b68593a5180dd33c135a",
+    "url": "/Poesy_challenge/static/js/1.b68593a5.chunk.js"
+  },
+  {
+    "revision": "b5c3e78fbf753a2c6f9f",
+    "url": "/Poesy_challenge/static/js/runtime~main.b5c3e78f.js"
+  },
+  {
+    "revision": "bcadfa50d0f639b097e99ea17895e47e",
+    "url": "/Poesy_challenge/index.html"
+  }
+];
